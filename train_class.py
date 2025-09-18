829e3cb1e85282397904a367329004af59c5f87b
Agha-reza <rezaalizadeh84129@gmail.com>
2025-09-18 21:57:21 +0330

    class line 1

---
+++ train_class.py
@@ -0,0 +1,66 @@
+user = ""
+class Line:
+    def __init__(self):
+        self.lines = {}
+        self.unauthorized = []
+
+    def add_line(self):
+        while True:
+            line_name = input("please enter line_name: ")
+            if line_name in self.lines:
+                print("This name is duplicated, please try again.")
+            else:
+                break
+        origin = input("enter origin: ")
+        destination = input("enter destination: ")
+        #number = input("enter number: ")
+        stations_list = []
+        print("To create a list of stations, enter the name of each station, then press the enter button \nand press the f button to finish.")
+        while True:
+            station = input("please print station name for add to stations_list: ")
+            if station == "f":
+                break
+            else:
+                stations_list.append(station)
+        number = len(stations_list)
+        print(f"you are creating a new line in name {line_name} with origin: {origin} and destination: {destination} and staions list of {stations_list} and {number} stations")
+        while True:
+            ok = input("Is the information confirmed or are you rebuilding the line? \n"
+            "1) ok \n"
+            "2) I repeat the operation again. \n")
+            if ok == "1":
+                self.lines[line_name] = [origin, destination, number, stations_list]
+                break
+            elif ok == "2":
+                self.add_line()
+
+    def update_line(self):
+        line_name = input("input linename: ")
+        print(f"{line_name} line:\n \t"
+              f"origin: {self.lines[line_name][1]}\n"
+              f"destination: {self.lines[line_name][2]}\n"
+              f"number: {self.lines[line_name][3]}\n"
+              f"stations_list: {self.lines[line_name][4]}\n")
+        print("which one update? \n"
+              "1)linename \n"
+              "2)origin \n"
+              "3)destination \n"
+              "4)number \n"
+              "5)stationlist")
+        n = int(input())
+        new = input("please enter your new value: ")
+        if n == 1:
+            self.lines[new] = self.lines[line_name]
+            self.lines.pop(line_name)
+        elif n in range(2,6):
+            self.lines[line_name][n-2] = new
+
+    def delete_line(self):
+        line_name = input("enter linename to delete :")
+        self.lines.pop(line_name)
+        self.unauthorized.append(user)
+
+    def line_information(self):
+        print(self.lines)
+
+
