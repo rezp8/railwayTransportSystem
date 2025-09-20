4b8edc8218f45bd3c8090a69f5ed494c78fb73e7
Sara <saradaliri05@gmail.com>
2025-09-20 19:18:43 +0330

    added employee panel function

--- employee.py
+++ employee.py
@@ -18,10 +18,34 @@ def employee_login(employees):
         if matched_user:
             if matched_user["password"] == input_password:
                 print("Login was successful!")
+                todo=input("What would you like to do next?\n1.Add a line\n2.Update line information\n3.Delete a line\n4.View list of lines\n5.Add a train\n6.Update train information\n7.Delete a train\n8.View list of trains\n9.Log out\n")
+                employee_panel(todo)
                 return   
             else:
                 print("Incorrect password, please try again.\n")
         else:
             print("Username not found, please try again.\n")
+from line_class import Line
+from employee_class import Employee
+
+def employee_panel(todo):
+
+    match todo:
+        case "1" :
+            Line.add_line( )
+        case "2" :
+            Line.update_line()
+        case "3":
+            Line.delete_line()
+        case "4":
+            Line.line_information()
+        case "5":
+            Employee.add_train()
+        case "6":
+            Employee.edit_train()
+        case "7":
+            Employee.delete_train()
+        case "8":
+            Employee.trains_info()
 
 
